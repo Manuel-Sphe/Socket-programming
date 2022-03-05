4379d4e1375b64df6cb258f21c6a7f661fadcf97
Sphesihle <sphesihlemanuel99@gmail.com>
2022-03-05 10:53:55 +0200

    Updated user log in the No part

--- UDPClient.py
+++ UDPClient.py
@@ -1,11 +1,60 @@
 from socket import *
+serverName = gethostbyname(gethostname())
+serverPort = 13000
 
-serverName = 'hostname'
-serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+ADDR = (serverName, serverPort)
+print("<<Welcome to chat2Go>>")
+message = input("Already has an accout ?(Y or N) : ")
+message = message.upper()
 
-clientSocket = socket(AF_INET,SOCK_DGRAM)
-message = input("Input lowercase sentence:")
-clientSocket.sendto(message.encode(),(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+if(message == 'Y'):
+    clientSocket.sendto(message.encode(),ADDR)
+    #welcome messgae
+    welcome = clientSocket.recvfrom(2048)
+    print(welcome[0].decode(),end="")
+    
+    #Enter user name 
+    prompt1  = clientSocket.recvfrom(2048)
+    print(prompt1[0].decode())
+    name = input()
+    clientSocket.sendto(name.encode(),ADDR)
+    
+    #password
+    prompt2 = clientSocket.recvfrom(2048)
+    print(prompt2[0].decode())
+    password = input() 
+    clientSocket.sendto(password.encode(),ADDR)
+
+    #logged in massage
+    log  = clientSocket.recvfrom(2048)
+    print(log[0].decode())
+
+elif(message == 'N'):
+    #name 
+    clientSocket.sendto(message.encode(),ADDR)
+    name = clientSocket.recvfrom(2048)
+    print(name[0].decode())
+    n = input()
+    clientSocket.sendto(n.encode(),ADDR)
+
+    #password
+    pass_w = clientSocket.recvfrom(2048)
+    print(pass_w[0].decode())
+    p =input()
+    clientSocket.sendto(p.encode(),ADDR)
+
+    #sign up
+    su = clientSocket.recvfrom(2048)
+    print(su[0].decode())
+    
+    #confirm password
+    con = clientSocket.recvfrom(2048)
+    print(con[0].decode())
+    con_f = input()
+    clientSocket.sendto(con_f.encode(),ADDR)
+
+
+#modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+#print(modifiedMessage.decode())
 clientSocket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -1,10 +1,77 @@
 from socket import *
-serverPort = 12000
+serverPort = 13000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+welcome = "Welcome "
+host = gethostbyname(gethostname())
 
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
+
+def sign_UP(name,pass_word):
+    '''Adding a new user to the database'''
+    file = open("users.txt","a+")
+    su="Signing you up..."
+    serverSocket.sendto(su.encode(),clientAddress)
+
+    confirm = "Confirm your password:>"
+    serverSocket.sendto(confirm.encode(),clientAddress)
+    confirm = serverSocket.recvfrom(2048)
+    if(pass_word==confirm[0].decode()):
+        file.write(name+","+pass_word+"\n")
+    file.close()
+
+def verify(name,password):
+    flag = False
+    f = open("users.txt","r")
+    for line  in f:
+        s = line.split(",")
+        if(name==s[0] and password == s[1]):
+            flag = True
+    f.close()
+    return flag
+
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
+    print("Recieved from :",clientAddress[0])
+    # get the Y or N
     modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    if(modifiedMessage == 'Y'):
+        serverSocket.sendto(welcome.encode(),clientAddress)
+        n = "Enter User Name (char and numbers):"
+        serverSocket.sendto(n.encode(),clientAddress)
+        name = serverSocket.recvfrom(2048)
+
+        pw = "Enter your password:"
+        serverSocket.sendto(pw.encode(),clientAddress)
+        pass_word = serverSocket.recvfrom(2048)
+        
+        log_in ="Logged in...."
+        if(verify(name[0].decode(),pass_word[0].decode())):
+           ## print("Logged in .....")
+            serverSocket.sendto(log_in.encode(),clientAddress)
+        else:
+            log_in = "Sorry Wrong Username or password Try again"
+            serverSocket.sendto(log_in.encode(),clientAddress)
+            #name = input("Enter User Name (char and numbers):\n>")
+            #pass_word = input("Enter your password:\n>")
+            
+    elif(modifiedMessage == 'N'):
+        #name
+        n = "Enter User Name (char and numbers):"
+        serverSocket.sendto(n.encode(),clientAddress)
+        name = serverSocket.recvfrom(2048)
+
+        #password
+        p ="Enter your password:"
+        serverSocket.sendto(p.encode(),clientAddress)
+        pass_word = serverSocket.recvfrom(2048)
+    
+
+        sign_UP(name[0].decode(),pass_word[0].decode())
+
+    
+   # serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+
+    #serverSocket.close()
