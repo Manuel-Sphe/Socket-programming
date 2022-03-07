f4eb7600e156844cfcde964f8f8a606e054d148f
Sphesihle Madonsela <49991613+Linusihle@users.noreply.github.com>
2022-03-07 21:49:31 +0200

    Threading added on the server side

--- UDPServer.py
+++ UDPServer.py
@@ -1,13 +1,21 @@
 from socket import *
+import threading
+import time
+
 serverPort = 13000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 welcome = "Welcome "
 host = gethostbyname(gethostname())
 
-serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
+serverSocket.bind((host, serverPort))
 
+def unmusk(bytes):
+    return bytes.decode()
+
+def musk(bytes):
+    return bytes.encode()
+    
 def sign_UP(name,pass_word):
     '''Adding a new user to the database'''
     file = open("users.txt","a+")
@@ -18,7 +26,7 @@ def sign_UP(name,pass_word):
     serverSocket.sendto(confirm.encode(),clientAddress)
     confirm = serverSocket.recvfrom(2048)
     if(pass_word==confirm[0].decode()):
-        file.write(name+","+pass_word+"\n")
+        file.write((name+","+pass_word)+"\n")
     file.close()
 
 def verify(name,password):
@@ -31,47 +39,65 @@ def verify(name,password):
     f.close()
     return flag
 
-while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    print("Recieved from :",clientAddress[0])
-    # get the Y or N
-    modifiedMessage = message.decode().upper()
-    if(modifiedMessage == 'Y'):
-        serverSocket.sendto(welcome.encode(),clientAddress)
-        n = "Enter User Name (char and numbers):"
-        serverSocket.sendto(n.encode(),clientAddress)
-        name = serverSocket.recvfrom(2048)
+def respond(text,client_address):
+    serverSocket.sendto(text.encode(),client_address)
+    pass
 
+def handle_client(data,client_adrress):
+    '''Handle the client'''
+    #handle the rerequent
+    # the first thing we are expecting is the 'Y' or 'N'
+    print("I'm ready to communicate now")
+
+    message = data.decode()
+    if(message=='Y'):
+        respond(welcome,client_adrress)
+        n = "Enter User Name (char and numbers):"
+        respond(n,client_adrress)
+        name = serverSocket.recvfrom(2048)
+        
         pw = "Enter your password:"
-        serverSocket.sendto(pw.encode(),clientAddress)
+        respond(pw,client_adrress)
         pass_word = serverSocket.recvfrom(2048)
         
-        log_in ="Logged in...."
-        if(verify(name[0].decode(),pass_word[0].decode())):
-           ## print("Logged in .....")
-            serverSocket.sendto(log_in.encode(),clientAddress)
+        
+        log_in = "Logged in...."
+        if(verify(unmusk(name[0]),unmusk(pass_word[0]))):
+            #print Logged in 
+            respond(log_in,client_adrress)
         else:
-            log_in = "Sorry Wrong Username or password Try again"
-            serverSocket.sendto(log_in.encode(),clientAddress)
-            #name = input("Enter User Name (char and numbers):\n>")
-            #pass_word = input("Enter your password:\n>")
-            
-    elif(modifiedMessage == 'N'):
-        #name
+            log_in ="Sorry Wrong Username or password Try again"
+            respond(log_in,client_adrress)
+        time.sleep(0.5)
+    elif(message == 'N'):
+        #name 
         n = "Enter User Name (char and numbers):"
-        serverSocket.sendto(n.encode(),clientAddress)
+        respond(n,client_adrress)
         name = serverSocket.recvfrom(2048)
-
-        #password
-        p ="Enter your password:"
-        serverSocket.sendto(p.encode(),clientAddress)
+        
+        p = "Enter your password"
+        
+        respond(p,client_adrress)
+        
         pass_word = serverSocket.recvfrom(2048)
-    
+        
+        sign_UP(unmusk(name[0]),unmusk(pass_word[0]))
+        
+    # now we need to handle the message sending
 
-        sign_UP(name[0].decode(),pass_word[0].decode())
-
-    
-   # serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-
-
-    #serverSocket.close()
+    pass
+while True:
+    try:
+        try:
+            message, clientAddress = serverSocket.recvfrom(2048)
+            handle_client(message,clientAddress)
+            my_thread = threading.Thread(target=handle_client,args=(message,clientAddress))
+            my_thread.start()
+            print("Message sent now !!!")
+        except OSError:
+            print("Failed to read message from client")
+            break
+            
+    except KeyboardInterrupt:
+        print("Shutting down server")
+        serverSocket.close()
